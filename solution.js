60ca9f80dd9b79ee76566541a0deeb71d0dcdcc0
Aaqib <drop.mohamedaaqib@gmail.com>
2025-12-01 16:20:33 +0530

    write the psuedocode

---
+++ solution.js
@@ -0,0 +1,11 @@
+// PSEUDOCODE: Assign jobs to agents based on priority
+//
+// 1. Loop through array of the objects
+// 2. Create an  array for job titles
+// 3. Create an array  for job agents
+// 4. Loop through the job_titles array and sort the jobtitles based on urgent tags
+// 5. Create an empty array
+// 6.Assign for each job_title based on priority to a job_agent who has a primary skill
+//  * if the agent doesnt have primary skill check it in the secondary skills or else move to the next agent
+//   7. Return the Output array
+
