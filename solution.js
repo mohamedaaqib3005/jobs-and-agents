ccd761becd096dcb38f89d3158ddda63d9a33f0b
Aaqib <drop.mohamedaaqib@gmail.com>
2025-12-15 15:39:09 +0530

    make it declarative

--- solution.js
+++ solution.js
@@ -13,35 +13,32 @@ const { readFileSync } = require('fs');
 
 const input = JSON.parse(readFileSync('sample-data/input.json', 'utf8'));
 console.log("the input", input)
+
+function parseInput(input, key) {
+  return input
+    .filter(object => object[key])
+    .map(object => object[key])
+}
+
 function assignJobs(input) {
-  const agents = input// create a func called parseInput it should not be more than WET
-    .filter(object => object.new_agent)
-    .map(object => object.new_agent);
-
-  const requests = input
-    .filter(object => object.job_request)
-    .map(object => object.job_request);
-
-  const jobs = input
-    .filter(object => object.new_job)
-    .map(object => object.new_job)
+  const agents = parseInput(input, "new_agent");
+  const requests = parseInput(input, "job_request");
+  const jobs = parseInput(input, "new_job")
     .sort((a, b) => b.urgent - a.urgent);
 
-  return requests.reduce((assignments, { agent_id }) => {//change to map
-    const agent = agents.find(a => a.id === agent_id); // create a func findAgentbyid
+  function findAgentById(agent_id) {
+    return agents.find(a => a.id === agent_id);
+  }
 
-    if (!agent) return assignments; // throw an error when there is no agent
 
-    // const match = jobs.find(job =>
-    //   job.type === agent.primary_skillset[0]//prioritise primary skillset and then check secondary skillset
-    //   || job.type === agent.secondary_skillset[0]
-    // );// create func matchJobs
+  return requests.reduce((assignments, { agent_id }) => {
+    const agent = findAgentById(agent_id)
+    if (!agent) throw new Error("agent not available");
 
-    const primarymatch = jobs.find((job) => agent.primary_skillset.includes(job.type))
+    const primaryMatch = jobs.find((job) => agent.primary_skillset.includes(job.type))
 
-    const secondarymatch = jobs.find((job) => agent.secondary_skillset.includes(job.type))
+    const match = primaryMatch || jobs.find((job) => agent.secondary_skillset.includes(job.type))
 
-    const match = primarymatch || secondarymatch;
     if (!match) return assignments;
 
     jobs.splice(jobs.indexOf(match), 1);
@@ -54,4 +51,3 @@ function assignJobs(input) {
 
 console.log(assignJobs(input))
 
-// make changes based on the  rule 5 only assign if it has primary skillset
